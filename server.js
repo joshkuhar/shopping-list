522eff72a7945901a62a6f988cbd96895699d131
Josh Kuhar <jdkuhar@gmail.com>
2016-08-20 09:42:11 -0400

    update

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var Storage = function() {
+    this.items = [];
+    this.id = 0;
+};
+
+Storage.prototype.add = function(name) {
+    var item = {name: name, id: this.id};
+    this.items.push(item);
+    this.id += 1;
+    return item;
+};
+
+var storage = new Storage();
+storage.add('Broad beans');
+storage.add('Tomatoes');
+storage.add('Peppers');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/items', function(request, response) {
+    response.json(storage.items);
+});
+
+app.listen(8080);
