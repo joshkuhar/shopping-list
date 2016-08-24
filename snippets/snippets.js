cd045ae1e80ab830a6eb05b858ea8338905808b8
Josh Kuhar <jdkuhar@gmail.com>
2016-08-24 11:20:31 -0400

    added mongodb

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -7,6 +7,43 @@ MongoClient.connect('mongodb://localhost/', function(err, db) {
         db.close();
         return;
     }
-    console.log('Connected to MongoDB database');
-    db.close();
+
+    var collection = db.collection('snippets');
+
+    var create = function(name, content) {
+    	db.close();
+    };
+
+    var read = function(name, content) {
+    	db.close();
+    };
+
+    var update = function(name, content) {
+        db.close();
+    };
+
+    var del = function(name, content) {
+        db.close();
+    };
+
+    var main = function() {
+        if (process.argv[2] == 'create') {
+            create(process.argv[3], process.argv[4]);
+        }
+        else if (process.argv[2] == 'read') {
+            read(process.argv[3]);
+        }
+        else if (process.argv[2] == 'update') {
+            update(process.argv[3], process.argv[4]);
+        }
+        else if (process.argv[2] == 'delete') {
+            del(process.argv[3]);
+        }
+        else {
+            console.error('Command not recognized');
+            db.close();
+        }
+    };
+
+    main();
 });
